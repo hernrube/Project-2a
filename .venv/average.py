735bc22c9f0aacff5cbf15713cb7595d2c4fc79f
Ruben Hernandez <rubdog52014@gmail.com>
2024-07-04 14:39:55 -0700

    User enters 5 numbers and the average of those numbers are printed

---
+++ .venv/average.py
@@ -0,0 +1,15 @@
+#Author: Ruben Hernandez
+# GitHub username: hernrube
+# Date: 7/04/2024
+# Description: Asks the user to input five numbers and
+#              prints out the average of those numbers.
+
+print("Please enter five numbers.")
+num_1=float(input())
+num_2=float(input())
+num_3=float(input())
+num_4=float(input())
+num_5=float(input())
+avg=((num_1+num_2+num_3+num_4+num_5)/5)
+print("The average of those numbers is:")
+print(avg)
